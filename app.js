f1698019d094e07a038ed0ae7c1ad51944ec1d4c
loginov007 <147260705+loginov007@users.noreply.github.com>
2024-01-27 17:56:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const progress = document.getElementById('progress');
+const circles = document.querySelectorAll('.circle');
+
+let activeIndex = 1;
+
+prevBtn.addEventListener('click', () => {
+    activeIndex--;
+    if (activeIndex < 1) {
+        activeIndex = 1;
+    }
+
+    update();
+});
+
+nextBtn.addEventListener('click', () => {
+    activeIndex++;
+
+    if(activeIndex > circles.length) {
+        activeIndex = circles.length;
+    }
+    update();
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < activeIndex) {
+            circle.classList.add('active');
+        }else{
+            circle.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+    if (activeIndex === 1) {
+        prevBtn.disabled = true;
+    }else if (activeIndex === circles.length) {
+        nextBtn.disabled = true;
+    }else {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
+}
